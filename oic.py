e91e51e296d540edb50ccedab55dfe9b77c0cb6e
rajeshkrm123 <rajeshkrm123@gmail.com>
2022-01-02 16:36:19 +0400

    This commit to check the conflict

--- oic.py
+++ oic.py
@@ -2,3 +2,11 @@ The first commit was a blank file.
 This file blongs to oic for development and this line added after the first commit.
 
 This line added to check the commit difference between local and remote repo
+
+
+
+
+
+
+
+This line adding from local repo
