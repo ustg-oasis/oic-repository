8c8b09c49983e4603b3c47f4164281796b58d7c4
rajeshkrm123 <rajeshkrm123@gmail.com>
2022-01-02 16:40:00 +0400

    Pull and commit of oic.py

--- oic.py
+++ oic.py
@@ -2,14 +2,5 @@ The first commit was a blank file.
 This file blongs to oic for development and this line added after the first commit.
 This line added to check the commit difference between local and remote repo
 
-<<<<<<< HEAD
-
-
-
-
-
-
 This line adding from local repo
-=======
 Adding this file for the testing from website.
->>>>>>> d40b51cdd275601f487b9fb7e50813242dbeffb3
