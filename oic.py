e9afec23f1251fcc460436f18de3bd5d4b79bb49
rajeshkrm123 <rajeshkrm123@gmail.com>
2022-01-02 16:09:26 +0400

    This is latest commit

--- oic.py
+++ oic.py
@@ -1,3 +1,4 @@
 The first commit was a blank file.
 This file blongs to oic for development and this line added after the first commit.
 
+This line added to check the commit difference between local and remote repo
